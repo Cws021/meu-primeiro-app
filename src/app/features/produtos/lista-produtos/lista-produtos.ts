d32fb0831be0aa70d326ec7be6160b4c7b9caa25
Cawã Santos <cardosocawa28@gmail.com>
2026-08-07 11:30:30 -0300

    Aula de Router - 07/08

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -8,6 +8,8 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+  // SIGNAL
+
   // writable sognal => signal (relativo) que permite alterações (com set ou update)
   produtos = signal([
     { nome: 'Notebook', preco: 3800 },
@@ -15,29 +17,23 @@ export class ListaProdutos {
     { nome: 'Fone', preco: 80 },
   ]);
 
+  produtoSelecionado = signal<string | null>(null);
+
+  carrinho = signal<{ nome: string; preco: number }[]>([]);
+
+  // COMPUTED
+
   totalProdutos = computed(() => this.produtos().length); // computed signal observa outro signal e se atualiza automaticamente
 
   valorTotal = computed(() => {
     return this.produtos().reduce((total, item) => total + item.preco, 0);
   });
 
-  exibirProduto(nome: string) {
-    this.produtoSelecionado.set(nome);
-    // Aqui você pode atualizar o estado, abrir modal, etc.
-  }
+  quantidadeCarrinho = computed(() => this.carrinho().length);
 
-  produtoSelecionado = signal<string | null>(null);
-
-  //update  -> adiciona um item ao writable signal
-  adicionarProduto() {
-    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Teclado', preco: 250 }]);
-  }
-
-  // set -> altera um item do weitable signal
-  substituirProdutos() {
-    this.produtos.set([{ nome: 'Produto novo', preco: 999 }]);
-  }
-
+  totalCarrinho = computed(() => {
+    return this.carrinho().reduce((total, item) => total + item.preco, 0);
+  });
 
   // método construtor - formata os objetos criados a partir dessa classe
   constructor() {
@@ -57,7 +53,26 @@ export class ListaProdutos {
         document.title = `(${this.totalProdutos()}) Minha Loja`;
       }
     });
-
   } //fim do contructo
+
   //estes effects  gera mensagens no terminal sempre que alterações são realizadas
+
+  exibirProduto(nome: string) {
+    this.produtoSelecionado.set(nome);
+    // Aqui você pode atualizar o estado, abrir modal, etc.
+  }
+
+  //update  -> adiciona um item ao writable signal
+  adicionarProduto() {
+    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Teclado', preco: 250 }]);
+  }
+
+  // set -> altera um item do weitable signal
+  substituirProdutos() {
+    this.produtos.set([{ nome: 'Produto novo', preco: 999 }]);
+  }
+
+  adicionarAoCarrinho(produto: { nome: string; preco: number }) {
+    this.carrinho.update((listaAtual) => [...listaAtual, produto]);
+  }
 }
