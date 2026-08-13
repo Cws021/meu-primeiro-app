fd8e49fae5a45a1206447af2f9b0d442735fcd4a
Cawã Santos <cardosocawa28@gmail.com>
2026-08-13 10:15:42 -0300

    http client e consumo de API - service

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,5 +1,6 @@
 import { CanActivateFn } from '@angular/router';
-import { usuarioLogado } from './auth';
-export const authGuard: CanActivateFn = () => {
-  return usuarioLogado();
+  import { usuarioLogado } from './auth';
+
+  export const authGuard: CanActivateFn = () => {
+    return usuarioLogado();
 };
--- src/app/core/auth.ts
+++ src/app/core/auth.ts
@@ -1,12 +1,11 @@
 import { signal } from '@angular/core';
 
-export const usuarioLogado = signal(false);
+  export const usuarioLogado = signal(false);
 
-// core/auth.ts (continuação)
-export function login() {
-  usuarioLogado.set(true);
-}
-export function logout() {
-  usuarioLogado.set(false);
-}
+   export function login() { 
+    usuarioLogado.set(true);
+  }    
 
+  export function logout() {
+    usuarioLogado.set(false);
+  }
