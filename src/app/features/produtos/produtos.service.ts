bf27a208b360a533ace27d859e56973d4abab178
Cawã Santos <cardosocawa28@gmail.com>
2026-08-13 11:04:15 -0300

    Interceptor - 13/08

--- src/app/features/produtos/produtos.service.ts
+++ src/app/features/produtos/produtos.service.ts
@@ -15,7 +15,7 @@ type Produto = {
 export class ProdutosService {
   private http = inject(HttpClient);
 
-  private API = 'https://fakestoreapi.com/products';
+  private API = 'https://fakestoreapi.com/products-erro';
 
   buscarProdutos() {
     return this.http.get<ProdutoApi[]>(this.API);
