45ff242b6b326be92d3c4d9a99f7dd67c1bba951
Cawã Santos <cardosocawa28@gmail.com>
2026-08-19 10:12:46 -0300

    Mudando coisas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -22,7 +22,7 @@ export const routes: Routes = [
     canActivate: [authGuard],
     loadComponent: () => import('./features/checkout/checkout/checkout').then((m) => m.Checkout),
   },
-  {
+    {
     path: 'admin',
     canActivate: [adminGuard],
     loadComponent: () => import('./features/admin/admin/admin').then((m) => m.Admin),
@@ -32,12 +32,13 @@ export const routes: Routes = [
     loadComponent: () => import('./features/login/login/login').then((m) => m.Login),
   },
   {
-    path: '**',
-    redirectTo: '', // redireciona para a página inicial
-  },
-  {
     path: 'acesso-negado',
     loadComponent: () =>
       import('./features/acesso-negado/acesso-negado/acesso-negado').then((m) => m.AcessoNegado),
   },
+  {
+    path: '**',
+    redirectTo: '', // redireciona para a página inicial
+  }
+
 ];
